2f32382a8c3caf831e76dc442c0a00aeb34a69a1
catursetyo <catursetyo26@gmail.com>
2026-03-06 13:27:52 +0700

    test abstraction

--- abstraction/src/App.java
+++ abstraction/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        Car gtr = new Car("gtr", "white");
+
         System.out.println("Hello, World!");
     }
 }
