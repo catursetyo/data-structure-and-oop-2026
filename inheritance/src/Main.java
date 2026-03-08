a4faf94fb49500766f49429043da3b3bea7b5745
catursetyo <catursetyo26@gmail.com>
2026-03-08 07:56:27 +0700

    finished and compiled Main

---
+++ inheritance/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Gacoan caur = new Gacoan("sukidi", "rendang", 1000);
+
+        System.out.println("I'm currently:");
+        caur.cook();
+    }
+}
