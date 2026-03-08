39e50b95c14605bcf7ac9fdf3c878d8e5756f400
catursetyo <catursetyo26@gmail.com>
2026-03-08 08:45:50 +0700

    finished and compiled

--- encapsulation/src/Main.java
+++ encapsulation/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Noodle indomie = new Noodle("indomie", "rendang", 3000);
+
+        indomie.setFlavor("matcha");
+        indomie.setPrice(1500);
+
+        System.out.println("Flavor: " + indomie.getFlavor() + "\n" + "Price: " + indomie.getPrice());
     }
 }
